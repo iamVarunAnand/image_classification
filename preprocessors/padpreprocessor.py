62b5788858f468368cdc7affc7ddbd76544916d8
Varun <varunmct@yahoo.co.in>
2020-07-14 23:17:05 +0530

    updated pad preprocessor

---
+++ preprocessors/padpreprocessor.py
@@ -0,0 +1,22 @@
+# import the necessary packages
+import numpy as np
+
+
+class PadPreprocessor:
+    """
+    Zero pads the input image by the specified amount
+    (Support only for padding both height and width by the same amount)
+
+    Attributes
+    ----------
+    pad: int
+        amount of padding to add to both height and width of the image
+    """
+
+    def __init__(self, pad):
+        # initialize the instance variables
+        self.pad = pad
+
+    def preprocess(self, img):
+        # return the padded image
+        return np.pad(img, ((self.pad, self.pad), (self.pad, self.pad), (0, 0)))
