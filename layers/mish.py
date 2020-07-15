7c25a16064bafface0492a923ce848dd519d6b16
Varun <varunmct@yahoo.co.in>
2020-07-15 15:54:22 +0530

    added mish support

---
+++ layers/mish.py
@@ -0,0 +1,33 @@
+# import the necessary packages
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as K
+
+
+class Mish(Layer):
+    """
+    Mish Activation Function. (https://arxiv.org/abs/1908.08681)
+    .. math::
+        mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + e^{x}))
+    Shape:
+        - Input: Arbitrary. Use the keyword argument `input_shape`
+        (tuple of integers, does not include the samples axis)
+        when using this layer as the first layer in a model.
+        - Output: Same shape as the input.
+    Examples:
+        >>> X_input = Input(input_shape)
+        >>> X = Mish()(X_input)
+    """
+
+    def __init__(self, **kwargs):
+        super(Mish, self).__init__(**kwargs)
+        self.supports_masking = True
+
+    def call(self, inputs):
+        return inputs * K.tanh(K.softplus(inputs))
+
+    def get_config(self):
+        config = super(Mish, self).get_config()
+        return config
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
